070a7c05484d6eb5a9d858dba403f7079dc8e95c
Arjun Chib <arjun@callrail.com>
2025-10-24 11:13:29 -0400

    Run prettier --write

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-function add(a:number,  b:number) {
-return a+ b;
+function add(a: number, b: number) {
+  return a + b;
 }
 
 const x = 5;
 const y = 10;
 
-console.log(`${x} + ${y} = ${add(x,y)}`)
+console.log(`${x} + ${y} = ${add(x, y)}`);
